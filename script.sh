9f5553001ea8bee33f73df5aa09d5637229066f8
ErPyrex <sergioalarcon22986@gmail.com>
2023-05-27 16:58:37 -0400

    fix: bug con las animaciones

--- script.sh
+++ script.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# Función para mostrar una barra de progreso
-function show_progress() {
-    local duration=$1
-    local sleep_time=0.2
-    local elapsed_time=0
-    local progress_char="█"
-    local progress_string=""
-    local max_chars=$(tput cols)
-    local max_ticks=$((duration / sleep_time))
-
-    for ((i = 0; i <= max_chars; i++)); do
-        progress_string+=" "
-    done
-
-    while ((elapsed_time <= duration)); do
-        elapsed_ticks=$((elapsed_time / sleep_time))
-        for ((i = 0; i <= elapsed_ticks; i++)); do
-            progress_string="${progress_string:0:i}$progress_char${progress_string:i+1}"
-        done
-        printf "\r[%s] %s%%" "$progress_string" "$((elapsed_ticks * 100 / max_ticks))"
-        sleep "$sleep_time"
-        elapsed_time=$((elapsed_time + sleep_time))
-    done
-
-    printf "\n"
-}
-
 # Actualizar el sistema
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -59,15 +32,6 @@ fi
 # Instalar kitty
 (curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n) &
 
-# Mostrar animación con parrot.live
-(curl -s parrot.live) &
-
-# Esperar a que las tareas en segundo plano finalicen
-wait
-
-# Limpiar la pantalla
-clear
-
 # Agregar kitty al PATH
 sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
 
@@ -95,12 +59,6 @@ chmod a+x ~/Desktop/kitty.desktop
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" &
 pid=$!
 
-# Mostrar barra de progreso para la instalación de Oh My Zsh
-show_progress 5
-
-# Esperar a que la instalación de Oh My Zsh finalice
-wait "$pid"
-
 # Clonar los repositorios de plugins para Zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -108,13 +66,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Instalar fzf
 (git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install) &
 
-# Mostrar animación con parrot.live
-(curl -s parrot.live) &
-
-# Esperar a que las tareas en segundo plano finalicen
-wait
-
-# Limpiar la pantalla
+# Limpiar pantalla para preguntar si nvim o vscode
 clear
 
 # Preguntar al usuario si desea instalar Neovim (nvim) o Visual Studio Code (vscode)
